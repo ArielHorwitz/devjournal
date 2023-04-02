31121e1005e6aa265d17efb95a980b370d1c37f7
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-04-02 18:00:18 +0300

    Rename app to DevJournal

--- src/app.rs
+++ src/app.rs
@@ -19,12 +19,12 @@ use tui::{backend::Backend, Terminal};
 const TICK_RATE_MS: u64 = 25;
 
 pub fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
-    let datadir = AppDirs::new(Some("devboard"), false)
+    let datadir = AppDirs::new(Some("devjournal"), false)
         .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "failed to create user folder"))?
         .data_dir;
     fs::create_dir_all(&datadir)?;
     let tick_rate = Duration::from_millis(TICK_RATE_MS);
-    let mut app_state = App::new("DevBoard", datadir);
+    let mut app_state = App::new("DevJournal", datadir);
     let mut last_tick = Instant::now();
     loop {
         terminal.draw(|frame| draw(frame, &app_state, false))?;
